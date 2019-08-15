2b218bb5395c22aabf10bdc0acfcaa7b64258471
absurd <827906643@qq.com>
2019-08-15 16:17:25 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  baseUrl: '/zex-mgr',
+  // baseUrl: '/zex-mgr',
   outputDir: 'zex-mgr', // 输出文件目录
   devServer: {
     port: 9999, // 端口号
-    host: '0.0.0.0',
+    host: '127.0.0.1',
     https: false, // https:{type:Boolean}
     open: true, // 配置自动启动浏览器
-    // proxy: null,
-    /*proxy: {
+    proxy: null,
+    proxy: {
       '/zex-mgr': {
         //   target: 'http://pre-release.sweetmartmarketing.com:8088',
         target: 'http://localhost:8088',
-        //  target: 'http://192.168.2.234:8088', 
+        //  target: 'http://192.168.2.234:8088',
         ws: true,
         changeOrigin: true
       },
       '/foo': {
         target: '<other_url>'
       }
-    }*/
+    }
 
   },
 
@@ -36,5 +36,5 @@ module.exports = {
 }
 
 
-// 打包时 修改线上地址 src/api/index.js 
-//  
+// 打包时 修改线上地址 src/api/index.js
+//
