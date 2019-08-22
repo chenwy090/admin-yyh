91cfe7aee3b0d55c7721382b135160918245dbd2
jian.chen <chenj@52iuh.com>
2019-08-22 21:17:34 +0800

    banner位table列表显示drawId

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,34 @@
+const webpack = require("webpack");
 module.exports = {
-    baseUrl: '/zex-mgr',
+    // baseUrl: '/zex-mgr',
     outputDir: 'zex-mgr', // 输出文件目录
+    configureWebpack: {
+        // 配置 webpack plugins
+        plugins: [
+            // 指定初次常量
+            new webpack.DefinePlugin({
+                '__UPDATED__': JSON.stringify(new Date().toLocaleString())
+            })
+        ]
+    },
     devServer: {
         port: 9999, // 端口号
         host: '0.0.0.0',
         https: false, // https:{type:Boolean}
         open: true, // 配置自动启动浏览器
-        // proxy: null,
-        // proxy: {
-        //     '/zex-mgr': {
-        //         //   target: 'http://pre-release.sweetmartmarketing.com:8088',
-        //         target: 'http://localhost:8088',
-        //         //  target: 'http://192.168.2.234:8088',
-        //         ws: true,
-        //         changeOrigin: true
-        //     },
-        //     '/foo': {
-        //         target: '<other_url>'
-        //     }
-        // }
+        proxy: null,
+        proxy: {
+            '/zex-mgr': {
+                //   target: 'http://pre-release.sweetmartmarketing.com:8088',
+                target: 'http://localhost:8088',
+                //  target: 'http://192.168.2.234:8088',
+                ws: true,
+                changeOrigin: true
+            },
+            '/foo': {
+                target: '<other_url>'
+            }
+        }
 
     },
 
