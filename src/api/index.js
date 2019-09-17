5ed175b53d5f39bd7d41e088f240c385f3d9fa42
jian.chen <chenj@52iuh.com>
2019-09-17 18:20:37 +0800

    新增/编辑优惠券添加第三方链接地址

--- src/api/index.js
+++ src/api/index.js
@@ -10,10 +10,12 @@ import {
 
 // 本地访问地址
 // export const baseUrl = "http://192.168.31.179:8088/zex-mgr";
+// export const baseUrl = "http://192.168.31.102:8088/zex-mgr";
+
 
 // 测试环境
-// export const baseUrl = "http://47.103.117.70:8088/zex-mgr";
-// export const downloadUrl = "http://47.103.117.70:8088/zex-mgr";
+export const baseUrl = "http://47.103.117.70:8088/zex-mgr";
+export const downloadUrl = "http://47.103.117.70:8088/zex-mgr";
 
 // dev开发环境 http://47.102.126.157:8088/zex-mgr/
 // export const baseUrl = "http://47.102.126.157:8088/zex-mgr";
@@ -24,8 +26,8 @@ import {
 // export const downloadUrl = "http://review.52iuh.com:8088/zex-mgr";
 
 // 正式环境 http://crm.52iuh.com:8088/zex-mgr
-export const baseUrl = "http://crm.52iuh.com:8088/zex-mgr";
-export const downloadUrl = "http://crm.52iuh.com:8088/zex-mgr";
+// export const baseUrl = "http://crm.52iuh.com:8088/zex-mgr";
+// export const downloadUrl = "http://crm.52iuh.com:8088/zex-mgr";
 
 
 
