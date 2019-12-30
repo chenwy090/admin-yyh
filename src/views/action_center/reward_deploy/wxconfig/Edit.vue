903972e790e978d8c9eed8bba9c4196a3a4377ec
chenjian <chenj@52iuh.com>
2019-12-30 18:35:29 +0800

    微信号动态表单

--- src/views/action_center/reward_deploy/wxconfig/Edit.vue
+++ src/views/action_center/reward_deploy/wxconfig/Edit.vue
@@ -21,44 +21,41 @@
           </Row>
         </FormItem>
 
-        <!-- _id: Matth.random(),
-        wxnumber: '',
-        qrcodeImgUrl: '',
-        defaultQrcodeImgUrlList: [],-->
-        <!-- :rules="{required: true, message: 'Item ' + item.index +' can not be empty', trigger: 'blur'}" -->
-        <FormItem
-          v-for="(item, index) in formDynamic.items"
-          :key="index"
-          :label="`微信号.${index+1}: `"
-          :prop="`items.${index}.wxnumber`"
-          :rules="{required: true,  validator: validateEmpty('请输入微信号')}"
-        >
-          <Row>
+        <template v-for="(item, index) in formDynamic.items">
+          <Row :key="index">
             <Col span="12">
-              <Input type="text" v-model.trim="item.wxnumber" placeholder="请输入微信号"></Input>
+              <FormItem
+                :label="`微信号.${index+1}: `"
+                :prop="`items.${index}.wxnumber`"
+                :rules="{required: true,  validator: validateEmpty('请输入微信号')}"
+              >
+                <Input
+                  type="text"
+                  style="width:80%"
+                  v-model.trim="item.wxnumber"
+                  placeholder="请输入微信号"
+                ></Input>
+              </FormItem>
             </Col>
-            <Col span="8">
-              <UploadImage
-                label="微信二维码："
-                :fileUploadType="'qrcodeImgUrl'"
-                :defaultList="item.defaultQrcodeImgUrlList"
-                @remove="removeQrcodeImgUrl(index)"
-                @uploadSuccess="qrcodeImgUrlUploadSuccess($event,index)"
-              ></UploadImage>
+            <Col span="6">
+              <FormItem
+                :label-width="0"
+                :prop="`items.${index}.qrcodeImgUrl`"
+                :rules="{ required: true, message: '请上传图片' }"
+              >
+                <UploadImage
+                  :fileUploadType="'qrcodeImgUrl'"
+                  :defaultList="item.defaultQrcodeImgUrlList"
+                  @remove="removeQrcodeImgUrl(index)"
+                  @uploadSuccess="qrcodeImgUrlUploadSuccess($event,index)"
+                ></UploadImage>
+              </FormItem>
             </Col>
-            <Col span="4" offset="1">
+            <Col span="4">
               <Button @click="handleRemove(index)">删除</Button>
             </Col>
           </Row>
-        </FormItem>
-        <!-- <FormItem
-          label="微信号:"
-          prop="wxnumber"
-          :rules="{required: true,  validator: validateEmpty('请输入标题')}"
-        >
-          <Input style="width:80%" v-model="formData.wxnumber" placeholder="请输入标题" clearable />
-        </FormItem>-->
-        <!-- qrCode -->
+        </template>
       </Form>
       <div slot="footer">
         <Button type="error" @click="handleSubmit('form')">确认</Button>
@@ -69,14 +66,14 @@
 </template>
 
 <script>
-import util from '@/libs/util'
-import { postRequest } from '@/libs/axios'
-import createFormData from './createFormData'
+import util from "@/libs/util";
+import { postRequest } from "@/libs/axios";
+import createFormData from "./createFormData";
 
-import UploadImage from './UploadImage'
+import UploadImage from "./UploadImage";
 
 export default {
-  name: 'wxconfig-edit',
+  name: "wxconfig-edit",
   components: { UploadImage },
   props: {
     showEdit: {
@@ -87,8 +84,8 @@ export default {
       type: Object,
       default: () => ({
         _id: Math.random(),
-        title: '添加微信号',
-        type: 'edit', //add/edit/detail/audit
+        title: "添加微信号",
+        type: "edit", //add/edit/detail/audit
         data: {},
       }),
     },
@@ -96,17 +93,17 @@ export default {
   watch: {
     action: {
       handler() {
-        let { type, data } = this.action
+        let { type, data } = this.action;
         // this.isShow = true;
-        console.log('watch action')
+        console.log("watch action");
         // 新增
-        if (type == 'add') {
-          this.url = '/activityInfo/add'
-          this.formData = createFormData()
+        if (type == "add") {
+          this.url = "/activityInfo/add";
+          this.formData = createFormData();
         } else {
           //edit 修改
-          this.url = '/activityInfo/edit'
-          this.formData = JSON.parse(JSON.stringify(data))
+          this.url = "/activityInfo/edit";
+          this.formData = JSON.parse(JSON.stringify(data));
         }
       },
       deep: true,
@@ -116,7 +113,7 @@ export default {
     return {
       ruleValidate: {},
       modal: {
-        levelName: '提现-开关',
+        levelName: "提现-开关",
         isopen: false,
       },
       isShow: true,
@@ -125,108 +122,107 @@ export default {
         items: [
           {
             _id: Math.random(),
-            wxnumber: '',
-            qrcodeImgUrl: '',
+            wxnumber: "",
+            qrcodeImgUrl: "",
             defaultQrcodeImgUrlList: [],
           },
         ],
       },
-    }
+    };
   },
   created() {},
   methods: {
-    removeQrcodeImgUrl() {
-      console.log('removeQrcodeImgUrl', index)
-
-      let item = this.formData.items[index]
-      item.qrcodeImgUrl = ''
-      item.defaultQrcodeImgUrlList = []
-      this.formData.items.splice(index, 1, item)
+    removeQrcodeImgUrl(index) {
+      console.log("removeQrcodeImgUrl", index);
+      let item = this.formDynamic.items[index];
+      item.qrcodeImgUrl = "";
+      item.defaultQrcodeImgUrlList = [];
+      this.formDynamic.items.splice(index, 1, item);
     },
     qrcodeImgUrlUploadSuccess({ imgUrl }, index) {
-      console.log('qrcodeImgUrlUploadSuccess', imgUrl, index)
-      let item = this.formData.items[index]
-      item.qrcodeImgUrl = imgUrl
-      item.defaultQrcodeImgUrlList = [{ imgUrl }]
-      this.formData.items.splice(index, 1, item)
+      console.log("qrcodeImgUrlUploadSuccess", imgUrl, index);
+      let item = this.formDynamic.items[index];
+      item.qrcodeImgUrl = imgUrl;
+      item.defaultQrcodeImgUrlList = [{ imgUrl }];
+      this.formDynamic.items.splice(index, 1, item);
     },
     closeDialog() {
       //关闭对话框清除表单数据
-      console.log('closeDialog')
-      this.isShow = false
-      this.$emit('update:showEdit', false)
+      console.log("closeDialog");
+      this.isShow = false;
+      this.$emit("update:showEdit", false);
     },
 
     handleAdd() {
       this.formDynamic.items.push({
         _id: Math.random(),
-        wxnumber: '',
-        qrcodeImgUrl: '',
+        wxnumber: "",
+        qrcodeImgUrl: "",
         defaultQrcodeImgUrlList: [],
-      })
+      });
     },
     handleRemove(index) {
-      this.formDynamic.items.splice(index, 1)
+      this.formDynamic.items.splice(index, 1);
       // this.$emit("del", index);
     },
     handleSubmit(name) {
       this.$refs[name].validate(async valid => {
         // console.log(JSON.stringify(this.formValidate));
         if (valid) {
-          this.msgOk('数据验证成功!')
-          let oForm = JSON.parse(JSON.stringify(this.formData))
-          let { code, msg } = await postRequest(this.url, oForm)
+          this.msgOk("数据验证成功!");
+          let oForm = JSON.parse(JSON.stringify(this.formDynamic));
+          let { code, msg } = await postRequest(this.url, oForm);
 
           if (code == 200) {
-            this.msgOk('保存成功')
+            this.msgOk("保存成功");
             // 关闭对话框
-            this.closeDialog()
+            this.closeDialog();
             //刷新列表数据
-            this.$emit('refresh')
+            this.$emit("refresh");
           } else {
-            this.msgErr(msg)
+            this.msgErr(msg);
           }
         } else {
-          this.msgErr('数据验证失败,请检查表单!')
+          this.msgErr("数据验证失败,请检查表单!");
         }
-      })
+      });
     },
     validateEmpty(msg, len = 20) {
       return function(rule, value, callback) {
-        value += ''
-        value = value.trim()
-        if (value == '') {
-          return callback(msg)
+        value += "";
+        value = value.trim();
+        if (value == "") {
+          return callback(msg);
         }
-        let length = util.getByteLen(value)
+        let length = util.getByteLen(value);
         if (length > len * 2) {
-          return callback(`最多只能输入${len}个汉字`)
+          return callback(`最多只能输入${len}个汉字`);
         }
-        callback()
-      }
+        callback();
+      };
     },
     validateWxList(rule, value, callback) {
-      console.log('validateWxList', rule, value)
+      console.log("validateWxList", rule, value);
       if (!value.length) {
-        return callback('请添加微信号')
+        return callback("请添加微信号");
       }
-      callback()
+      callback();
     },
     // 全局提示
     msgOk(txt) {
       this.$Message.info({
         content: txt,
         duration: 3,
-      })
+      });
     },
     msgErr(txt) {
       this.$Message.error({
         content: txt,
         duration: 3,
-      })
+      });
     },
   },
-}
+};
 </script>
 
 <style scoped>
