4c055c59f8e910ff148fac8dc30e3cb576edb5bf
chenjian <chenj@52iuh.com>
2020-01-02 09:16:39 +0800

    add内容专区需求

--- src/views/action_center/reward_deploy/wxconfig/createFormData.js
+++ src/views/action_center/reward_deploy/wxconfig/createFormData.js
@@ -1,10 +1,16 @@
 export default function createFormData() {
     return {
+        // 最多添加100个微信号
         items: [
             {
                 _id: Math.random(),
-                wxnumber: '',
-                qrcodeImgUrl: '',
+                // 序号
+                // 配置项：添加微信号
+                // 配置值 取第一个微信的微信号
+                // 修改人
+                // 修改时间
+                wxnumber: '', //微信号
+                qrcodeImgUrl: '',// 微信二维码
                 defaultQrcodeImgUrlList: [],
             },
         ],
