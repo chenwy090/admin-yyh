fc318715e8402bf6da39d2c9452b4a352942a561
moka <gaoyuanfell@sina.com>
2020-01-14 17:13:54 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -1,38 +1,41 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import iView from 'iview'
-import App from './App'
-import { router } from './router/index'
-import store from './store'
-import '@/locale'
-import 'iview/dist/styles/iview.css' //
-import VueI18n from 'vue-i18n';
-import Icon from 'vue-awesome/components/Icon'
+import Vue from "vue";
+import App from "./App";
+import { router } from "./router/index";
+import store from "./store";
+import "@/locale";
+import "@/styles/index.less";
+import VueI18n from "vue-i18n";
+import Icon from "vue-awesome/components/Icon";
+Vue.component("icon", Icon);
 
-import '@/styles/index.less'
+import iView from "iview";
+import "iview/dist/styles/iview.css"; //
+Vue.use(iView);
 
-import hasRole from '@/libs/hasRole'
-import { getRequest, postRequest, putRequest, deleteRequest, uploadFileRequest } from '@/libs/axios'
-import { setStore, getStore, removeStore } from '@/libs/storage'
-import util from '@/libs/util';
-import hasPermission from '@/libs/hasPermission';
-import TreeTable from 'tree-table-vue'
-import '@babel/polyfill'
+// import ViewUI from "view-design";
+// import "view-design/dist/styles/iview.css";
+// Vue.use(ViewUI);
 
-import './directive/num-formatter'
+import hasRole from "@/libs/hasRole";
+import { getRequest, postRequest, putRequest, deleteRequest, uploadFileRequest } from "@/libs/axios";
+import { setStore, getStore, removeStore } from "@/libs/storage";
+import util from "@/libs/util";
+import hasPermission from "@/libs/hasPermission";
+import TreeTable from "tree-table-vue";
+import "@babel/polyfill";
+
+import "./directive/num-formatter";
 import "@/libs/mathUtil"; //添加数学方法扩充函数
 
-import hotzone from 'vue-hotzone';
-import VueClipboard from 'vue-clipboard2'
+import hotzone from "vue-hotzone";
+import VueClipboard from "vue-clipboard2";
 
+Vue.config.productionTip = false;
 
-
-Vue.config.productionTip = false
-Vue.use(iView);
 Vue.use(VueI18n);
 Vue.use(TreeTable);
-Vue.component('icon', Icon);
 Vue.use(hasPermission);
 Vue.use(hasRole);
 Vue.use(hotzone);
@@ -50,19 +53,19 @@ Vue.prototype.removeStore = removeStore;
 
 /* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    router,
-    store,
-    render: h => h(App),
-    data: {
-        currentPageName: ''
-    },
-    mounted() {
-        // 初始化菜单
-        util.initRouter(this);
-        this.currentPageName = this.$route.name;
-        // 显示打开的页面的列表
-        this.$store.commit('setOpenedList');
-        this.$store.commit('initCachepage');
-    }
-})
+  el: "#app",
+  router,
+  store,
+  render: h => h(App),
+  data: {
+    currentPageName: "",
+  },
+  mounted() {
+    // 初始化菜单
+    util.initRouter(this);
+    this.currentPageName = this.$route.name;
+    // 显示打开的页面的列表
+    this.$store.commit("setOpenedList");
+    this.$store.commit("initCachepage");
+  },
+});
